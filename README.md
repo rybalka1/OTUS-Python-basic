05850900d391c5b9497a098ed6c2c9a5449beb55
Дмитрий Рыбалка <rybalka@flexline.ru>
2021-03-16 18:04:07 +0300

    Поправил main и README.md на главной странице

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # OTUS-Python-basic
 
-Данный репозиторий будет содержать домашние задания по курсу "Python Developer. Basic" от OTUS.
+## Данный репозиторий будет содержать домашние задания по курсу "Python Developer. Basic" от OTUS.
+
+- [1-е задание  "Публикация репозитория"](<https://github.com/rybalka1/OTUS-Python-basic/tree/main/homework_01>)
--- homework_01/main.py
+++ homework_01/main.py
@@ -4,22 +4,31 @@
 """
 
 
-def power_numbers():
+def power_numbers(*args):
     """
     функция, которая принимает N целых чисел,
     и возвращает список квадратов этих чисел
     >>> power_numbers(1, 2, 5, 7)
     <<< [1, 4, 25, 49]
     """
+    return [i ** 2 for i in args]
 
+
+# test = power_numbers(1, 2, 5, 7)
+# print(test)
 
 # filter types
 ODD = "odd"
 EVEN = "even"
 PRIME = "prime"
 
+def is_prime(n):
+    d = 2
+    while d * d <= n and n % d != 0:
+        d += 1
+    return d * d > n
 
-def filter_numbers():
+def filter_numbers(ls, param):
     """
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
@@ -30,3 +39,9 @@ def filter_numbers():
     >>> filter_numbers([2, 3, 4, 5], EVEN)
     <<< [2, 4]
     """
+    res = []
+    for item in ls:
+        res.append(item) if param == 'odd' and item % 2 != 0 else None
+        res.append(item) if param == 'even' and item % 2 == 0 else None
+        res.append(item) if param == 'prime' and is_prime(item) else None
+    return res
